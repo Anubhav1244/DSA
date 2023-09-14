9310871574f6f9fcfe810ad55d11f54f12360999
Anubhav1244 <1rn21cs026.anubhavkumar@gmail.com>
2023-09-14 13:57:43 +0530

    first commit

---
+++ Array/sort_0_1.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int arr[100]={1,1,0,0,1,1,1,0,1};
+    int count0=0;
+    int count1=0;
+    for(int i=0;i<9;i++)
+    {
+        if(arr[i]==0)
+        {
+            count0++;
+        }
+        else
+        {
+            count1++;
+        }
+    }
+    
+    for(int i=0;i<count0;i++)
+    {
+        arr[i]=0;
+
+    }
+    for(int j=count0;j<9;j++)
+    {
+        arr[j]=1;
+    }
+    for(int i=0;i<count0+count1;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    
+
+}
